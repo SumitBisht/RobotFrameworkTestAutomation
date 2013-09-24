7877dabd0967534350ec8fe1e7ca994780cf82d0
Sumit Bisht <sumitbisht1987@gmail.com>
2013-09-24 19:46:32 +0530

    added python hash

--- Ch2/Variables/testsuites/variables.py
+++ Ch2/Variables/testsuites/variables.py
@@ -3,3 +3,4 @@ Weather = [ "London", "Cloudy", 25]
 humidity = 75
 MARKS = 65.5
 DAYS = ["Monday", "Wednesday", "Friday", "Sunday"]
+details = {'genre':'open-source','type':'test automation', 'pub':2013}
